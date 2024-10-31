57d92973d31b92fec8f0ce3c384db23501fcd485
TalalImran07 <reachtalalimran@gmail.com>
2024-10-31 17:01:24 +0500

    Fixed Errors

--- modules.py
+++ modules.py
@@ -1,5 +1,7 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+
 
 class DoubleConv(nn.Module):
     def __init__(self, in_channels, out_channels, mid_channels = None, residual = False):
